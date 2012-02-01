9ed1b7e6c1f1d8b515ea5343a4037d9958a20569
Krister Axel <krister.axel@gmail.com>
2012-02-01 02:26:49 -0800

    slight progress

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@
  */
 
 exports.index = function(req, res){
-	
+  var rc = req.params.runcode;
   var exchange = require('exchange-rates');
+  
 
   exchange.load('http://openexchangerates.org/historical/2006-02-16.json', function(error, data) {
     // etc...
     var jsonObject = JSON.parse(data);
-    res.render('index', { title: 'Erate.TV', day_rate: jsonObject});
+    
+    res.render('index', { title: 'Erate.TV', day_rate: jsonObject, runcode: rc});
 
-  });
+        });
 };
