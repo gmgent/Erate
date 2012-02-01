3836de29e9ecd829beba0ee7cd2e1a754796ebf5
Krister Axel <krister.axel@gmail.com>
2012-02-01 01:06:08 -0800

    add money js and parse JSON

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,13 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+	
+  var exchange = require('exchange-rates');
+
+  exchange.load('http://openexchangerates.org/historical/2006-02-16.json', function(error, data) {
+    // etc...
+    var jsonObject = JSON.parse(data);
+    res.render('index', { title: 'Erate.TV', day_rate: jsonObject});
+
+  });
 };
